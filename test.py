7df6027ac33f313edccb876a9be848c90eb7ae46
nithint29 <nithint29@gmail.com>
2017-05-23 12:02:31 -0400

    pictures

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print("Hi");
+import numpy as np
+import cv2
+
+# Load an color image in grayscale
+img = cv2.imread('lot.jpg',0)
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+cv2.__version__
