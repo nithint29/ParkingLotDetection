f11e079cc02df74b98366a39cc6eb7ec4d7b113a
marswatson <zsy397849514@gmail.com>
2017-06-16 14:22:49 -0400

    this file is used to load and store image from file folder

---
+++ dataset.py
@@ -0,0 +1,29 @@
+import numpy as np
+import cv2
+import glob
+from matplotlib import pyplot as plt
+import os
+
+HEIGHT = 200
+WIDTH = 124
+
+''' read images from a directory '''
+def loadFolder(folderPath):
+    img_list = []
+    for files in glob.glob(folderPath + "/*.jpg"):
+        img = cv2.imread(files, cv2.IMREAD_COLOR)
+        img_list.append(img)
+    return img_list
+
+
+''' read images from a directory, resize images and save '''
+def loadResizeSave(srcPath, savePath):
+    for i, imgs in enumerate(glob.glob(srcPath + "/*.jpg")):
+        img = cv2.imread(imgs)
+        img_resize = cv2.resize(img, (WIDTH, HEIGHT), interpolation=cv2.INTER_CUBIC)
+        cv2.imwrite(savePath + "/" + str(i + 1) + ".jpg", img_resize)
+    return
+
+# resize_empty_list = loadResizeSave("dataset/empty","dataset_resize/empty")
+# resize_occupied_list = loadResizeSave("dataset/occupied","dataset_resize/occupied")
+
