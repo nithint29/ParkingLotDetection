c5b97bdab0751f586620eb26cf5ceb21fe1a6a01
marswatson <zsy397849514@gmail.com>
2017-06-19 14:07:47 -0400

    resize dataset

--- dataset.py
+++ dataset.py
@@ -4,8 +4,8 @@ import glob
 from matplotlib import pyplot as plt
 import os
 
-HEIGHT = 200
-WIDTH = 124
+HEIGHT = 400
+WIDTH = 400
 
 ''' read images from a directory '''
 def loadFolder(folderPath):
@@ -21,9 +21,9 @@ def loadResizeSave(srcPath, savePath):
     for i, imgs in enumerate(glob.glob(srcPath + "/*.jpg")):
         img = cv2.imread(imgs)
         img_resize = cv2.resize(img, (WIDTH, HEIGHT), interpolation=cv2.INTER_CUBIC)
-        cv2.imwrite(savePath + "/" + str(i + 1) + ".jpg", img_resize)
+        cv2.imwrite(savePath + "/" + "{:03d}".format(i) + ".jpg", img_resize)
     return
 
-# resize_empty_list = loadResizeSave("dataset/empty","dataset_resize/empty")
-# resize_occupied_list = loadResizeSave("dataset/occupied","dataset_resize/occupied")
+resize_empty_list = loadResizeSave("dataset/empty","dataset_resize/empty")
+resize_occupied_list = loadResizeSave("dataset/occupied","dataset_resize/occupied")
 
